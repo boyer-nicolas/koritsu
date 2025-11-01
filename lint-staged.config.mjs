527be9ee80dbc9036f72e3172a401ddbc41a8bf6
Nicolas Boyer <boyer63nicolas@gmail.com>
2025-11-01 10:52:04 +0100

    feat: lint-staged

---
+++ lint-staged.config.mjs
@@ -0,0 +1,14 @@
+
+/**
+ * @filename: lint-staged.config.js
+ * @type {import('lint-staged').Configuration}
+ */
+export default {
+	"src/app.d.ts": ["bun run ./scripts/generate-env.ts"],
+	"**/*.ts": () => [
+		"bun run lint:fix",
+		"bun test --findRelatedTests",
+		"bun run check",
+	],
+};
+
